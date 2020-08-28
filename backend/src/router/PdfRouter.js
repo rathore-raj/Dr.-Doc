f912fabbd573ebbec6f9cfd5bef64f5d5571cdbf
Raj Rathore <rathoreraj725@gmail.com>
2020-08-28 11:39:56 +0530

    Added Error Handling In Routes

--- backend/src/router/PdfRouter.js
+++ backend/src/router/PdfRouter.js
@@ -1,40 +1,50 @@
 const express = require("express");
 const merge = require("easy-pdf-merge");
-const convertapi = require("convertapi")(process.env.CONVERTAPI_SECRET);//Add convertapi Secret Key
+const convertapi = require("convertapi")(process.env.CONVERTAPI_SECRET); //Add convertapi Secret Key
 const { upload } = require("../middleware/multer.js");
 const router = express.Router();
-const {uploads} = require('../middleware/multerForGV')
-var path = require('path');
+const { uploads } = require("../middleware/multerForGV");
+var path = require("path");
 var appDir = path.dirname(require.main.filename);
 const gVision = require("../js/gVision");
-const ilovepdfSDK = require('ilovepdf-sdk');
-const sdk = new ilovepdfSDK(process.env.ILOVEPDF_PROJECT_PUBLICKEY,process.env.ILOVEPDF_PROJECT_SECRETKEY);
+const ilovepdfSDK = require("ilovepdf-sdk");
+const sdk = new ilovepdfSDK(
+  process.env.ILOVEPDF_PROJECT_PUBLICKEY,
+  process.env.ILOVEPDF_PROJECT_SECRETKEY
+);
 
-
-
-router.post("/compression",upload.single("avatar"),async(req, res) => {
-  try{
-      const task = await sdk.createTask('compress');
-      await task.addFile(req.file.path);
-      await task.process({CompressionLevel:"extreme"});
-      await task.download(`${appDir}/public/output/compressed-${req.file.originalname}`);
-      console.log("Compression Successful")
-      res.send("Compression Successful")
-  }catch(e){
-    res.send({Error:e.message})
+router.post("/compression", upload.single("avatar"), async (req, res) => {
+  try {
+    const task = await sdk.createTask("compress");
+    await task.addFile(req.file.path);
+    await task.process({ CompressionLevel: "extreme" });
+    await task.download(
+      `${appDir}/public/output/compressed-${req.file.originalname}`
+    );
+    console.log("Compression Successful");
+    res.send("Compression Successful");
+  } catch (e) {
+    res.send({ Error: e.message });
   }
 });
 
-router.post("/merge",upload.array("avatar"),(req, res) => {
+router.post(
+  "/merge",
+  upload.array("avatar"),
+  (req, res) => {
     const paths = req.files.map((file) => file.path);
-    merge(paths,`${appDir}/public/output/Merged-${req.files[0].originalname}`,(err) => {
+    merge(
+      paths,
+      `${appDir}/public/output/Merged-${req.files[0].originalname}`,
+      (err) => {
         if (err) {
-         throw new Error(err)
+          throw new Error(err);
         }
         res.send("Success!");
       }
     );
-  },(error, req, res, next) => {
+  },
+  (error, req, res, next) => {
     res.status(400).send({
       error: error.message,
     });
@@ -42,41 +52,40 @@ router.post("/merge",upload.array("avatar"),(req, res) => {
 );
 
 router.post("/convert", upload.single("avatar"), (req, res) => {
-  if (
-    !req.file.originalname.match(/\.(doc||docx||ppt||odt||html||xlsv||xlsx)/)
-  ) {
+  if (!req.file.originalname.match(/\.(doc||docx||ppt||odt||html||xlsv||xlsx)/)) {
     console.log("Upload Document File!");
     throw new Error("Upload Document File!");
   } else {
     convertapi
-      .convert("pdf", { File: req.file.path})
+      .convert("pdf", { File: req.file.path })
       .then((result) => {
         console.log("Result", result.file.fileInfo);
         res.send({ File_Download_Link: result.file.fileInfo.Url });
       })
       .catch((error) => {
-        res.status(400).send({ error: error.message });
+        res.status(400).json({ error: error.message });
       });
   }
+},(error, req, res, next) => {
+  res.status(400).send({
+    error: error.message,
+  });
 });
 
-
-router.post("/upload", uploads, async(req, res, next) => {
-
-  try{
+router.post("/upload", uploads, async (req, res, next) => {
+  try {
     const text = await gVision(req.file.path);
     // fs.unlink(req.file.path, err => {
     //     if (err) return console.log(err);
     //     console.log('photo deleted');
     // });
     res.status(200).json({
-        text, 
-        filename: req.file.filename
-    }); 
-  }catch(e){
-    res.send({Error:e.message})
+      text,
+      filename: req.file.filename,
+    });
+  } catch (e) {
+    res.send({ Error: e.message });
   }
-  
 });
 
 module.exports = router;
--- backend/src/router/user.js
+++ backend/src/router/user.js
@@ -1,87 +1,87 @@
-const express = require('express')
-const User = require('../model/user.js')
-const auth =require('../middleware/auth.js')
-const axios = require('axios')
-const router = express.Router()
+const express = require("express");
+const User = require("../model/user.js");
+const auth = require("../middleware/auth.js");
+const axios = require("axios");
+const router = express.Router();
 
+router.post("/users", async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+});
 
-router.post('/users', async (req, res) => {
-    const user = new User(req.body)
-    try {
-        await user.save()
-        const token = await user.generateAuthToken()
-        res.status(201).send({ user, token })
-    } catch (e) {
-        res.status(400).send(e.message)
-    }
-})
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (e) {
+    res.status(400).send("Unable To Login:" + e);
+  }
+});
 
-router.post('/users/login', async (req, res) => {
-    try {
-        const user = await User.findByCredentials(req.body.email, req.body.password)
-        const token = await user.generateAuthToken()
-        res.send({ user, token })
-    } catch (e) {
-        res.status(400).send('Unable To Login:'+e)
-    }
-})
+router.post("/users/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token != req.token;
+    });
+    await req.user.save();
+    res.send("Done!");
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
 
-router.post('/users/logout',auth, async(req, res)=>{
-    try{
-            req.user.tokens = req.user.tokens.filter((token)=>{
-                return token.token != req.token
-            })
-            await req.user.save()
-            res.send()
-    } catch(e){
-            res.status(500).send(e)
-    }
-})
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user);
+});
 
-router.get('/users/me', auth,async (req, res) => {
-    res.send(req.user)
-})
+router.patch("/users/me", auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["username", "email", "password"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
 
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
 
-router.patch('/users/me', auth ,async (req, res) => {
-const updates = Object.keys(req.body)
-const allowedUpdates = ['username', 'email', 'password']
-const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+  try {
+    updates.forEach((update) => (req.user[update] = req.body[update]));
+    await req.user.save();
 
-if (!isValidOperation) {
-    return res.status(400).send({ error: 'Invalid updates!' })
-}
+    res.send(req.user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 
-try {
-    updates.forEach((update) => req.user[update] = req.body[update])
-    await req.user.save()
+router.post("/users/logoutAll", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send("Done😃");
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
 
-    res.send(req.user)
-} catch (e) {
-    res.status(400).send(e)
-}
-})
+router.delete("/users/me", auth, async (req, res) => {
+  try {
+    await req.user.remove();
+    res.send("Deleted!");
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
 
-router.post('/users/logoutAll',auth, async (req, res)=>{
-    try{
-        req.user.tokens =[]
-        await req.user.save()
-        res.send("Done😃")
-    } catch(e){
-        res.status(500).send(e)
-    }
-})
-
-router.delete('/users/me',auth,async(req,res)=>{
-   try{
-       await req.user.remove()
-       res.send("Deleted!")
-   }catch(e){
-       res.status(500).send(e)
-   }
-})
-
-
-
-
-module.exports = router
+module.exports = router;
